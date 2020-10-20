adbd6d8c1eea85a6824a1f41ca2c2bcba9d49769
Gedders666122 <j.gedlek@hotmail.co.uk>
2020-10-20 14:48:42 +0100

    tailwind setup

--- routes/web.php
+++ routes/web.php
@@ -44,3 +44,7 @@ Route::get('update', function () {
 Route::get('delete', function () {
     return view('delete');
 });
+Auth::routes();
+
+Route::get('/home', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
